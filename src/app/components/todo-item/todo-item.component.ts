1cb6ef0f5388992b576849e5fb448a1f0f406580
newmohib <@@mohib100@@>
2024-12-20 12:49:43 +0600

    added custom directive and parants to child and child to parents data update and event trigger

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Todo } from '../../model/todo.type';
+import { HighlightCompletedTodoDirective } from '../../directives/highlight-completed-todo.directive';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [HighlightCompletedTodoDirective],
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.css'
+  styleUrl: './todo-item.component.css',
 })
 export class TodoItemComponent {
+  todo = input.required<Todo>();
+  todoToggled = output<Todo>();
 
+  todoClick() {
+    this.todoToggled.emit(this.todo());
+  }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,28 +2,42 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
+import { TodoItemComponent } from '../components/todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-
+  imports: [TodoItemComponent],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
-  todoItems = signal<Array<Todo>>([])
+  todoItems = signal<Array<Todo>>([]);
   ngOnInit(): void {
     // console.log(this.todoService.todoItems);
-    this.todoService.getTodosFromAPi().pipe(
-      catchError((err) => {
-        console.log(err);
-        return [];
-      })
-    ).subscribe((todos) => {
-      this.todoItems.set(todos)
-    })
-    
-  }
+    this.todoService
+      .getTodosFromAPi()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          return [];
+        })
+      )
+      .subscribe((todos) => {
+        this.todoItems.set(todos);
+      });
 
+    // this.todoItems.set(this.todoService.todoItems);
+  }
+  updateTodoItem(todoItem: Todo) {
+    this.todoItems.update((todos) => {
+      return todos.map((todo) => {
+        if (todo.id === todoItem.id) {
+          return { ...todo, completed: !todoItem.completed };
+        }
+        return todo;
+      });
+    });
+  }
 }
